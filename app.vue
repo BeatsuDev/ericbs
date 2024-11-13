18aca7c09459cd4a5a173cdaf84f4c52f37a3ea6
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-11-13 04:07:26 +0100

    chore: add tailwindcss

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div class="bg-gray-900 text-normal w-full min-h-dvh">
+        <NuxtRouteAnnouncer />
+    </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
+export default {
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+    ],
+    theme: {
+        colors: {
+            gray: colors.neutral,
+            text: colors.neutral,
+            primary: colors.emerald,
+            secondary: colors.cyan,
+        },
+        extend: {},
+    },
+    plugins: [],
+};
