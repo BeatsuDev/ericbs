784aedf143ca22e0270ba223e261066acd3f8ad5
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-12-01 20:38:30 +0100

    feat: add page SEO meta

--- app.vue
+++ app.vue
@@ -6,3 +6,23 @@
         </NuxtLayout>
     </div>
 </template>
+
+<script setup lang="ts">
+const descriptionString = `
+    I'm Eric Bieszczad-Stie, an enthusiastic software engineer in his early 20s who really
+    enjoys programming! I especially enjoy working with optimizing performance and readability
+    and learning about larger system architectures. I strive to share genuinely helpful
+    explanations and insights. Feel free to contact me about anything IT-related, social or 
+    anything in between! 🎉 Wish you all well.    
+`;
+
+useSeoMeta({
+    title: "Eric's Personal Website",
+    description: descriptionString,
+    charset: "utf-8",
+    ogLocale: "en_GB",
+    ogImage: "/assets/imgs/me-on-banana.webp",
+    ogUrl: "https://ericbs.com",
+    ogType: "website",
+})
+</script>
