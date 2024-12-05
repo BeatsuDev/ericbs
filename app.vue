0bed6a8c4f7d9f893914370c3d939ca4810d7710
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-12-05 14:13:35 +0100

    chore(SEO): add ogDescription and ogTitle

--- app.vue
+++ app.vue
@@ -8,17 +8,14 @@
 </template>
 
 <script setup lang="ts">
-const descriptionString = `
-    I'm Eric Bieszczad-Stie, an enthusiastic software engineer in his early 20s who really
-    enjoys programming! I especially enjoy working with optimizing performance and readability
-    and learning about larger system architectures. I strive to share genuinely helpful
-    explanations and insights. Feel free to contact me about anything IT-related, social or 
-    anything in between! 🎉 Wish you all well.    
-`;
+const title = "Eric's Personal Website";
+const descriptionString = "I'm Eric Bieszczad-Stie, an enthusiastic software engineer! I especially enjoy working with optimizing performance and readability. I strive to share genuinely helpful explanations and insights. Feel free to contact me about anything IT-related, social or anything in between! 🎉 Wish you all well.";
 
 useSeoMeta({
-    title: "Eric's Personal Website",
+    title: title,
+    ogTitle: title,
     description: descriptionString,
+    ogDescription: descriptionString,
     charset: "utf-8",
     ogLocale: "en_GB",
     ogImage: "https://ericbs.com/images/me-on-banana.webp",
